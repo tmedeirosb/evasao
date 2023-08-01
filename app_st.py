2bc70fc39df53fb28faa2025d2922e4f694cdc87
tmedeirosb <thiago.mdeiros@gmail.com>
2023-08-01 14:34:03 +0000

    Atualizando o st com nova base

--- app_st.py
+++ app_st.py
@@ -4,21 +4,40 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import datetime
 
 # Load the data
 df = pd.read_csv("Relatorio-dados-2.csv")
 
+# Define a function to categorize status
+def categorize_status(row):
+    today_year = datetime.datetime.now().year
+
+    if row['Situação no Curso'] == 'Evasão':
+        return 'Evasão'
+    elif row['Situação no Curso'] in ['Matriculado', 'Matricula Vinculo']:
+        if row['Ano Letivo de Previsão de Conclusão'] > today_year:
+            return 'Retenção'
+        else:
+            return 'Regular'
+    else:
+        return 'Outro'
+
+# Apply the function to create the new 'Status' column
+df['Status'] = df.apply(categorize_status, axis=1)
+
 # Define the options for the attribute selection
-attributes_options = ['Código Curso', 'Campus', 'Descrição do Curso', 'Ano Letivo de Previsão de Conclusão', 'Ano de Ingresso', 'Período Atual', 'Modalidade']
+attributes_options = ['Código Curso', 'Campus', 'Descrição do Curso', 'Ano Letivo de Previsão de Conclusão', 'Ano de Ingresso', 'Período Atual', 'Modalidade', 'Status']
 
 # Add 'Nenhum' to the options
 attribute_options_with_none = ['Nenhum'] + attributes_options
 
-# Add a selectbox for the user to choose the modalidade
+# Add a selectbox for the user to choose the modalidade and tipo de escola de origem
 modalidade = st.sidebar.selectbox('Selecione a modalidade:', df['Modalidade'].unique())
+tipo_escola_origem = st.sidebar.selectbox('Selecione o tipo de escola de origem:', df['Tipo de Escola de Origem'].unique())
 
-# Filter the data based on the selected modalidade
-df = df[df['Modalidade'] == modalidade]
+# Filter the data based on the selected modalidade and tipo de escola de origem
+df = df[(df['Modalidade'] == modalidade) & (df['Tipo de Escola de Origem'] == tipo_escola_origem)]
 
 # Add a selectbox for the user to choose between absolute values and percentage
 values_or_percentage = st.sidebar.selectbox('Selecione a forma de exibição:', ['Valores Absolutos', 'Porcentagem'])
@@ -37,17 +56,17 @@ if visualizar:
     # If the user selects only 1 attribute
     if attribute2 == 'Nenhum':
         if values_or_percentage == 'Valores Absolutos':
-            data_to_plot = df.groupby(attribute1)['Situação no Curso'].value_counts().unstack().fillna(0)
+            data_to_plot = df.groupby(attribute1)['Status'].value_counts().unstack().fillna(0)
         else:
-            data_to_plot = df.groupby(attribute1)['Situação no Curso'].value_counts(normalize=True).unstack().fillna(0)
-        data_to_plot['Evasão'].plot(kind='bar', ax=ax)
-        ax.set_title('Evasão por ' + attribute1)
+            data_to_plot = df.groupby(attribute1)['Status'].value_counts(normalize=True).unstack().fillna(0)
+        data_to_plot.plot(kind='bar', ax=ax)
+        ax.set_title('Status por ' + attribute1)
         ax.set_xlabel(attribute1)
-        ax.set_ylabel('Evasão')
+        ax.set_ylabel('Status')
         for container in ax.containers:
             ax.bar_label(container)
         # Add a horizontal line for the mean
-        ax.axhline(y=data_to_plot['Evasão'].mean(), color='r', linestyle='--')
+        ax.axhline(y=data_to_plot.mean().mean(), color='r', linestyle='--')
 
     # If the user selects 2 attributes
     else:
@@ -57,9 +76,9 @@ if visualizar:
             df_grouped = df.groupby([attribute1, attribute2]).size().unstack(fill_value=0)
             df_grouped = df_grouped.divide(df_grouped.sum(axis=1), axis=0)
             df_grouped.plot(kind='bar', stacked=True, ax=ax)
-        ax.set_title('Evasão por ' + attribute1 + ' e ' + attribute2)
+        ax.set_title('Status por ' + attribute1 + ' e ' + attribute2)
         ax.set_xlabel(attribute1)
-        ax.set_ylabel('Evasão')
+        ax.set_ylabel('Status')
         for container in ax.containers:
             ax.bar_label(container)
         # Add a horizontal line for the mean
@@ -67,5 +86,3 @@ if visualizar:
 
     # Show the plot
     st.pyplot(fig)
-
-
