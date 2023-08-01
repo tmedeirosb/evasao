2ff5b76319f5892115518bec23ab628cef8e5dc4
tmedeirosb <thiago.mdeiros@gmail.com>
2023-08-01 11:38:53 +0000

    Atualizando o st com nova base

--- app_st.py
+++ app_st.py
@@ -16,24 +16,29 @@ values_or_percentage = st.sidebar.selectbox('Selecione a forma de exibição:', 
 # Add a selectbox for the user to choose one or two attributes
 option = st.sidebar.selectbox('Selecione o número de atributos:', ['1', '2'])
 
-# If the user selects 1
-if option == '1':
-    attribute1 = st.sidebar.selectbox('Selecione o atributo:', df.columns)
-    if values_or_percentage == 'Valores Absolutos':
-        df1 = df.groupby(attribute1)['Situação no Curso'].value_counts().unstack().fillna(0)
-    else:
-        df1 = df.groupby(attribute1)['Situação no Curso'].value_counts(normalize=True).unstack().fillna(0)
-    df1['Evasão'].plot(kind='bar')
+# Add a "Visualizar" button
+visualizar = st.sidebar.button('Visualizar')
 
-# If the user selects 2
-else:
-    attribute1 = st.sidebar.selectbox('Selecione o primeiro atributo:', df.columns)
-    attribute2 = st.sidebar.selectbox('Selecione o segundo atributo:', df.columns)
-    if values_or_percentage == 'Valores Absolutos':
-        df2 = df.groupby([attribute1, attribute2])['Situação no Curso'].value_counts().unstack().fillna(0)
-    else:
-        df2 = df.groupby([attribute1, attribute2])['Situação no Curso'].value_counts(normalize=True).unstack().fillna(0)
-    df2['Evasão'].plot(kind='bar')
+# If the "Visualizar" button is pressed
+if visualizar:
+    # If the user selects 1
+    if option == '1':
+        attribute1 = st.sidebar.selectbox('Selecione o atributo:', df.columns)
+        if values_or_percentage == 'Valores Absolutos':
+            df1 = df.groupby(attribute1)['Situação no Curso'].value_counts().unstack().fillna(0)
+        else:
+            df1 = df.groupby(attribute1)['Situação no Curso'].value_counts(normalize=True).unstack().fillna(0)
+        df1['Evasão'].plot(kind='bar')
 
-# Show the plot
-st.pyplot()
+    # If the user selects 2
+    else:
+        attribute1 = st.sidebar.selectbox('Selecione o primeiro atributo:', df.columns)
+        attribute2 = st.sidebar.selectbox('Selecione o segundo atributo:', df.columns)
+        if values_or_percentage == 'Valores Absolutos':
+            df2 = df.groupby([attribute1, attribute2])['Situação no Curso'].value_counts().unstack().fillna(0)
+        else:
+            df2 = df.groupby([attribute1, attribute2])['Situação no Curso'].value_counts(normalize=True).unstack().fillna(0)
+        df2['Evasão'].plot(kind='bar')
+
+    # Show the plot
+    st.pyplot()
