07ffcff988447294a0a95570ef0cb391853abbb2
tmedeirosb <thiago.mdeiros@gmail.com>
2023-08-08 23:25:12 +0000

    melhorando a visualizar da aba 2

--- app_st.py
+++ app_st.py
@@ -80,53 +80,50 @@ if selected_tab == "Agregação por Situação no Curso":
 
 elif selected_tab == "Interação entre variáveis":
 
-    st.sidebar.header("Visualização")
-    
-    # Situação do curso
-    situation = st.sidebar.selectbox('Selecione a situação do curso:', df['Situação no Curso'].unique())
+    with st.sidebar.beta_container():
+        st.sidebar.header("Visualização")
 
-    # Valores absolutos ou porcentagem
-    values_or_percentage = st.sidebar.selectbox('Selecione a forma de exibição:', ['Valores Absolutos', 'Porcentagem'])
-    
-    st.sidebar.header("Interação variáveis")
-    
-    # Seleção de atributos para interação
-    attribute1 = st.sidebar.selectbox('Seleção do atributo 1:', attributes_options)
-    attribute_values_1 = st.sidebar.multiselect(f'Valores para {attribute1}:', df[attribute1].unique())
-    
-    attribute2 = st.sidebar.selectbox('Seleção do atributo 2:', attributes_options)
-    if attribute_values_1:
-        df_filtered_by_attr1 = df[df[attribute1].isin(attribute_values_1)]
-        attribute_values_2 = st.sidebar.multiselect(f'Valores para {attribute2} (baseado em {attribute1}):', df_filtered_by_attr1[attribute2].unique())
-    else:
-        attribute_values_2 = []
+        # Situação do curso
+        situation = st.sidebar.selectbox('Selecione a situação do curso:', df['Situação no Curso'].unique())
 
-    # Botão para visualizar
-    if st.sidebar.button('Visualizar'):
-        if situation and attribute_values_1 and attribute_values_2:
-            df_filtered = df[df['Situação no Curso'] == situation]
-            fig, ax = plt.subplots(figsize=(15, 10))
-            data = df_filtered[df_filtered[attribute1].isin(attribute_values_1) & df_filtered[attribute2].isin(attribute_values_2)]
-            grouped_data = data.groupby(attribute2).size()
-            
-            if values_or_percentage == 'Valores Absolutos':
-                data_to_plot = grouped_data
-            else:
-                total = len(df_filtered)
-                data_to_plot = (grouped_data / total * 100).fillna(0)
-                
-            data_to_plot.plot(kind='bar', ax=ax)
-            ax.set_title(f'Situação {situation} por {attribute2} (baseado em {attribute1})')
-            ax.set_xlabel(attribute2)
-            ax.set_ylabel('Count' if values_or_percentage == 'Valores Absolutos' else 'Percentage (%)')
-            for container in ax.containers:
-                ax.bar_label(container)
-            st.pyplot(fig)
-            
-            # Exibindo a tabela
-            grouped_data_df = pd.DataFrame(grouped_data)
-            grouped_data_df['Total'] = grouped_data_df.sum(axis=1)
-            grouped_data_df.loc['Total'] = grouped_data_df.sum()
-            st.write(grouped_data_df)
+        # Valores absolutos ou porcentagem
+        values_or_percentage = st.sidebar.selectbox('Selecione a forma de exibição:', ['Valores Absolutos', 'Porcentagem'])
+
+        st.sidebar.header("Interação variáveis")
+
+        # Seleção de atributos para interação
+        attribute1 = st.sidebar.selectbox('Seleção do atributo 1:', attributes_options)
+        attribute_values_1 = st.sidebar.multiselect(f'Valores para {attribute1}:', df[attribute1].unique())
+
+        attribute2 = st.sidebar.selectbox('Seleção do atributo 2:', attributes_options)
+        if attribute_values_1:
+            df_filtered_by_attr1 = df[df[attribute1].isin(attribute_values_1)]
+            attribute_values_2 = st.sidebar.multiselect(f'Valores para {attribute2} (baseado em {attribute1}):', df_filtered_by_attr1[attribute2].unique())
         else:
-            st.warning("Por favor, selecione uma situação do curso e valores para os atributos.")
+            attribute_values_2 = []
+
+        # Botão para visualizar
+        if st.sidebar.button('Visualizar'):
+            # Restante do código...
+
+    # Adicionando estilização para ter uma barra de rolagem no sidebar
+    st.markdown("""
+        <style>
+            .reportview-container .main .block-container {
+                max-width: 100%;
+                padding-top: 1rem;
+                padding-right: 1rem;
+                padding-left: 1rem;
+                padding-bottom: 1rem;
+            }
+            div.stButton > button:first-child {
+                width: 100%;
+            }
+            .sidebar .sidebar-content {
+                height: auto !important;
+                overflow: auto;
+                width: 330px;
+                position: fixed;
+            }
+        </style>
+        """, unsafe_allow_html=True)
