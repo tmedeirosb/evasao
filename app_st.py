67da23955026178dafb7dc9c6a0151e0fdf3de59
tmedeirosb <thiago.mdeiros@gmail.com>
2023-08-08 23:19:04 +0000

    removendo filtros da segunda aba

--- app_st.py
+++ app_st.py
@@ -79,54 +79,54 @@ if selected_tab == "Agregação por Situação no Curso":
             st.write(table_data)
 
 elif selected_tab == "Interação entre variáveis":
-    st.sidebar.header("Filtrar base")
-    filters = {attribute: st.sidebar.selectbox(f'Selecione {attribute}:', ['Nenhum'] + list(df[attribute].unique())) for attribute in attributes_options}
-    df_filtered = df.copy()
-    for attribute, value in filters.items():
-        if value != 'Nenhum':
-            df_filtered = df_filtered[df_filtered[attribute] == value]
-    
+
     st.sidebar.header("Visualização")
+    
     # Situação do curso
-    situations = st.sidebar.multiselect('Selecione as situações do curso:', df_filtered['Situação no Curso'].unique())
+    situation = st.sidebar.selectbox('Selecione a situação do curso:', df['Situação no Curso'].unique())
 
     # Valores absolutos ou porcentagem
     values_or_percentage = st.sidebar.selectbox('Selecione a forma de exibição:', ['Valores Absolutos', 'Porcentagem'])
     
     st.sidebar.header("Interação variáveis")
+    
     # Seleção de atributos para interação
     attribute1 = st.sidebar.selectbox('Seleção do atributo 1:', attributes_options)
-    attribute_values_1 = st.sidebar.multiselect(f'Valores para {attribute1}:', df_filtered[attribute1].unique())
+    attribute_values_1 = st.sidebar.multiselect(f'Valores para {attribute1}:', df[attribute1].unique())
     
     attribute2 = st.sidebar.selectbox('Seleção do atributo 2:', attributes_options)
     if attribute_values_1:
-        df_filtered_by_attr1 = df_filtered[df_filtered[attribute1].isin(attribute_values_1)]
+        df_filtered_by_attr1 = df[df[attribute1].isin(attribute_values_1)]
         attribute_values_2 = st.sidebar.multiselect(f'Valores para {attribute2} (baseado em {attribute1}):', df_filtered_by_attr1[attribute2].unique())
     else:
         attribute_values_2 = []
 
     # Botão para visualizar
     if st.sidebar.button('Visualizar'):
-        if situations and attribute_values_1 and attribute_values_2:
-            df_filtered = df_filtered[df_filtered['Situação no Curso'].isin(situations)]
+        if situation and attribute_values_1 and attribute_values_2:
+            df_filtered = df[df['Situação no Curso'] == situation]
             fig, ax = plt.subplots(figsize=(15, 10))
             data = df_filtered[df_filtered[attribute1].isin(attribute_values_1) & df_filtered[attribute2].isin(attribute_values_2)]
-            grouped_data = data.groupby(attribute2)['Situação no Curso'].value_counts().unstack().fillna(0)
+            grouped_data = data.groupby(attribute2).size()
+            
             if values_or_percentage == 'Valores Absolutos':
                 data_to_plot = grouped_data
             else:
-                total_by_group = grouped_data.sum(axis=1)
-                data_to_plot = (grouped_data.T / total_by_group * 100).T.fillna(0)
+                total = len(df_filtered)
+                data_to_plot = (grouped_data / total * 100).fillna(0)
+                
             data_to_plot.plot(kind='bar', ax=ax)
-            ax.set_title(f'Situação no Curso por {attribute2} (baseado em {attribute1})')
+            ax.set_title(f'Situação {situation} por {attribute2} (baseado em {attribute1})')
             ax.set_xlabel(attribute2)
             ax.set_ylabel('Count' if values_or_percentage == 'Valores Absolutos' else 'Percentage (%)')
             for container in ax.containers:
                 ax.bar_label(container)
             st.pyplot(fig)
+            
             # Exibindo a tabela
-            grouped_data['Total'] = grouped_data.sum(axis=1)
-            grouped_data.loc['Total'] = grouped_data.sum()
-            st.write(grouped_data)
+            grouped_data_df = pd.DataFrame(grouped_data)
+            grouped_data_df['Total'] = grouped_data_df.sum(axis=1)
+            grouped_data_df.loc['Total'] = grouped_data_df.sum()
+            st.write(grouped_data_df)
         else:
-            st.warning("Por favor, selecione ao menos uma situação do curso e valores para os atributos.")
+            st.warning("Por favor, selecione uma situação do curso e valores para os atributos.")
